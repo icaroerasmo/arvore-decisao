b2aa168fbd077612a3cdc506e0ce6f6f8b72cea4
Ícaro Erasmo Souza Barreiro <erasmo.iesb@gmail.com>
2021-05-03 00:28:16 -0300

    Refatoração

--- src/com/icaroerasmo/Main.java
+++ src/com/icaroerasmo/Main.java
@@ -5,7 +5,7 @@ import com.icaroerasmo.data.Database;
 public class Main {
 
 	public static void main(String[] args) {
-		Database db = Database.carregaDatabase("/home/icaroerasmo/Documentos/database2.csv");
+		Database db = Database.carregaDatabase("./database.csv");
 		var ganhos = db.calcularGanho();
 		for(String coluna : ganhos.keySet()) {
 			System.out.println(coluna + ": "+ganhos.get(coluna));
